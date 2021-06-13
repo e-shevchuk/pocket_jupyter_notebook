c7e687a8b0c344567157cb48575706df874c2c4e
Eugene Shevchuk <evgeniy.shevchuk@gmail.com>
2021-06-13 17:04:06 +0300

    Added installation script, upd launch script go.sh a bit

--- go.sh
+++ go.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# CONSTANTS
+
+REPO=jupyter_notebook
 # ACTIONS
 
 start_jupyter="start"
 stop_jupyter="stop"
-build_jupyter="up"
+up_jupyter="up"
 drop_jupyter="down"
 launch_bash="bash"
+build_jupyter="build"
 
 # COMMANDS
 
@@ -15,6 +19,7 @@ comm_down_jupyter=(docker-compose -f ~/jupyter/docker-compose.yml down)
 comm_stop_jupyter=(docker-compose -f ~/jupyter/docker-compose.yml stop)
 comm_start_jupyter=(docker-compose -f ~/jupyter/docker-compose.yml start)
 comm_bash_jupyter=(docker-compose --file ~/jupyter/docker-compose.yml exec notebook bash)
+comm_build_jupyter=(docker build -f ~/jupyter/Dockerfile -t $REPO ~/jupyter)
 
 # MESSAGES
 
@@ -22,18 +27,23 @@ help_l1=$'Usage: jpt <parameter>
 
     Parameters:
 
-    stop - stop service
+    stop  - stop service
     start - start service
-    up - create container
-    down - destroy container
+    up    - create container
+    down  - destroy container
+    build - destroy container
 
-    bash - log into the container with bash (if it is launched)
+    bash  - log into the container with bash (if it is launched)
 '
 
 # DOCKER MANIPULATIONS 
 
-# Up
+# Build
 if [ "$1" = "$build_jupyter" ]; then
+    "${comm_build_jupyter[@]}"
+
+# Up
+elif [ "$1" = "$up_jupyter" ]; then
     "${comm_up_jupyter[@]}"
 
 # Down
